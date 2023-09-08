920e73f0f710b64aaa9845d40decfbeb92222c02
Rockey Kumar <104070946+rockeykumarmuz@users.noreply.github.com>
2023-09-08 18:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n = str.length;
+	let freq = [26];
+
+	for(int i=0; i<n; i++) {
+		freq[s.charAt(i)-'a']++;
+	}
+
+	for(int i=0; i<n; i++) {
+		if(freq[s.charAt(i)-'a']>0 && freq[s.charAt(i)-'a']==1) {
+			return s.charAt(i);
+		}
+	} 
+	
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+ const input = prompt("Enter a string");
+ alert(firstNonRepeatedChar(input)); 
